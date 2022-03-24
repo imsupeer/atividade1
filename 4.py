220f206fa0d71bda6862008a0628fda605161c76
imsupeer <58670605+imsupeer@users.noreply.github.com>
2022-03-24 17:09:49 -0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,4 +1,7 @@
-#Faça um programa para a leitura de duas notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e apresentar: A mensagem "Aprovado", se a média alcançada for maior ou igual a sete; A mensagem "Reprovado", se a média for menor do que sete; A mensagem "Aprovado com Distinção", se a média for igual a dez.
+#Faça um programa para a leitura de duas notas parciais de um aluno. O programa deve calcular
+#a média alcançada por aluno e apresentar: A mensagem "Aprovado", se a média alcançada for maior
+#ou igual a sete; A mensagem "Reprovado", se a média for menor do que sete; A mensagem
+#"Aprovado com Distinção", se a média for igual a dez.
 
 nota1 = float(input("Insira a primeira nota do aluno: "))
 nota2 = float(input("Insira a segunda nota do aluno: "))
