5b5c1e707a09243762c24309785e669c53af69ad
imsupeer <58670605+imsupeer@users.noreply.github.com>
2022-03-24 17:11:03 -0300

    Update 7.py

--- 7.py
+++ 7.py
@@ -5,7 +5,9 @@
 # "Mora perto da vítima?" 
 # "Devia para a vítima?" 
 # "Já trabalhou com a vítima?" 
-# O programa deve no final emitir uma classificação sobre a participação da pessoa no crime. Se a pessoa responder positivamente a 2 questões ela deve ser classificada como "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como "Assassino". Caso contrário, ele será classificado como "Inocente".
+# O programa deve no final emitir uma classificação sobre a participação da pessoa no crime.
+#Se a pessoa responder positivamente a 2 questões ela deve ser classificada como "Suspeita",
+#entre 3 e 4 como "Cúmplice" e 5 como "Assassino". Caso contrário, ele será classificado como "Inocente".
 #   exemplo: res1 = int(input("Telefonou para a vítima? 1/Sim ou 0/Não: "))
 
 respostas_positivas = 0
